e26a2adc8770af9d693eaa7c601de4df3460d6ea
Jan Kängsepp <TA-19E-206@tptlive.ee>
2022-03-08 13:13:42 +0200

    added eslint with ruleset, enforced max-len and no console rules

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'import/extensions': 'off',
+    'no-restricted-globals': 'off',
+    'max-len': ['error', { code: 80 }],
+  },
+};
