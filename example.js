d1844e410f513d9026f4ef76c6538729f5012bc8
Artur Allmann <Artur.Allmann@tptlive.ee>
2022-03-08 14:50:33 +0200

    fixed lint errors

--- example.js
+++ example.js
@@ -12,7 +12,7 @@ console.log(chalk.yellow('9 + -5 ='), chalk.bold.green(sum(9, -5)));
 /* eslint-enable */
 // examples of testing using assert
 assert.equal(sum(2, 3), 5);
-assert.equal(sum(9, -5), 4)
+assert.equal(sum(9, -5), 4);
 assert.equal(sum(9, -5), 4);
 
 assert.notEqual(sum(1, 1), 1);
--- sum.test.js
+++ sum.test.js
@@ -1,8 +1,8 @@
-const sum = require("./sum.js")
+const sum = require('./sum.js');
 
-test("0 + 1 = 1", () =>{
-    expect(sum(0, 1)).toBe(1);
-})
-test("cannot add strings to numbers", () =>{
-    expect(() => {sum("mingi süna", 1)}).toThrow();
-})
+test('0 + 1 = 1', () => {
+  expect(sum(0, 1)).toBe(1);
+});
+test('cannot add strings to numbers', () => {
+  expect(() => { sum('mingi süna', 1); }).toThrow();
+});
