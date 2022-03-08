da3572864c35802e05bf5f2a14a6091af360fc0e
Ingela <59916738+ingeliza@users.noreply.github.com>
2022-03-08 13:47:44 +0200

    remove Number before .isNan

--- sum.js
+++ sum.js
@@ -1,8 +1,8 @@
 function sum(a, b) {
-  if (Number.isNaN(a)) {
+  if (isNaN(a)) {
     throw new Error("first parameeter is not number");
   }
-  if (Number.isNaN(b)) {
+  if (isNaN(b)) {
     throw new Error("2nd parameeter is not number");
   }
   return a + b;
