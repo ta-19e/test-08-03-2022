771ffc743f66eab491dceff2229977e99e2a67ca
Andres <Andres.Tammistu@tptlive.ee>
2022-03-08 14:44:31 +0200

    test that checks that exception to thrown when input is not number

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,11 @@
 const sum = require("./sum");
 
 test("adds 1 + 7 to equal 8", () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBe(3)
+  if (Number.isNaN(1)) {
+    throw new Error("first parameeter is not number")
+}
+if (Number.isNaN(7)) {
+    throw new Error("2nd parameeter is not number")
+}
 });
