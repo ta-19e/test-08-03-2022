b964a40cd6022325cdc681e16831765e300b9bc5
Andres <Andres.Tammistu@tptlive.ee>
2022-03-08 13:22:21 +0200

    Configured ESLint, airbnb ruleset and my rules

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        quotes: ["error", "double", "avoid-escape"],
+        'no-plusplus': 'off'
+    }
+}
