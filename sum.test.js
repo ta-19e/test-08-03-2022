60630b3c0eec1d9e8ad485debeb4ffa362fd0270
sink <59916608+sinkkkk@users.noreply.github.com>
2022-03-08 15:11:05 +0200

    errorid läinud

--- sum.test.js
+++ sum.test.js
@@ -1,8 +1,8 @@
-const sum = require("./sum.js")
+const sum = require("./sum");
 
-test("1 + 5 = 6", () =>{
-    expect(sum(1, 5)).toBe(6);
-})
-test("cannot add strings to numbers", () =>{
-    expect(() => {sum("word", 6)}).toThrow();
-})
+test("1 + 5 = 6", () => {
+  expect(sum(1, 5)).toBe(6);
+});
+test("cannot add strings to numbers", () => {
+  expect(() => { sum("word", 6); }).toThrow();
+});
