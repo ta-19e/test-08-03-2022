555cff73ff6b9f0a33a6424eb3a0f6db8319f910
Harri L <harri.liimann@gmail.com>
2022-03-08 13:25:14 +0200

    enforce eslint no-console and tab indent rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,18 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
-  rules: {
-  },
+	env: {
+		commonjs: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'airbnb-base',
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+	},
+	rules: {
+		'no-console': 'error',
+		indent: ['error', 'tab', 2],
+		'no-tabs': 'off',
+	},
 };
