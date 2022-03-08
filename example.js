7e61defd7655d77498b4fb0ec1b6a40bc8c902f5
Artur Allmann <Artur.Allmann@tptlive.ee>
2022-03-08 14:08:54 +0200

    reversed eslint fix changes

--- example.js
+++ example.js
@@ -1,11 +1,13 @@
 const chalk = require('chalk');
 const assert = require('assert');
 
+const sum = require('./sum.js');
 
 // test how function works
 /* eslint-disable */
 console.log(sum(1, 3));
 // use also colors to be more "fancy"
+console.log(chalk.yellow('2 + 3 ='), chalk.bold.green(sum(2, 3)));
 console.log(chalk.yellow('9 + -5 ='), chalk.bold.green(sum(9, -5)));
 /* eslint-enable */
 // examples of testing using assert
