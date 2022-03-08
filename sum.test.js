4f88d26c2aef1b7a6f55418f7ca08c502b8d00c0
sink <59916608+sinkkkk@users.noreply.github.com>
2022-03-08 14:30:22 +0200

    add test that checks that exception to thrown when input is not number

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,13 @@
 const sum = require('./sum');
 
 test('adds 1 + 5 to equal 6', () => {
-  expect(sum(1, 5)).toBe(6);
+    a = 1,
+    b = 5,
+  expect(sum(a, b)).toBe(6);
+if (isNaN(a)) {
+    throw new Error("first parameeter is not number")
+}
+if (isNaN(b)) {
+    throw new Error("2nd parameeter is not number")
+}
 });
