09d642d8abec3676587aa05ebb2ee8570acf28a6
Leib <allan.leibenau@tptlive.ee>
2022-03-08 14:34:13 +0200

    fix lint.yml issue

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,5 @@
-const sum = require('./sum')
+const sum = require('./sum');
 
-test('0 + 7 should = 7', () =>{
-    expect(sum (0, 7)).toBe(7);
+test('0 + 7 should = 7', () => {
+  expect(sum(0, 7)).toBe(7);
 });
