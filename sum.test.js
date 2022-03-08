ec6e54ace184e5cb07e61d04d69eb4fce103d729
Leib <allan.leibenau@tptlive.ee>
2022-03-08 15:01:31 +0200

    Update sum.test.js to throw error

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,9 @@
 const sum = require('./sum');
 
-test('0 + 7 should = 7', () => {
+test('0 + 7 should equal 7', () => {
   expect(sum(0, 7)).toBe(7);
+});
+
+test('b + 7 throw error', () => {
+    expect((sum(b, 7)).value()).toThrowError(Error);
 });
