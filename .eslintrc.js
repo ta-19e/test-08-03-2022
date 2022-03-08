67d38dfdc70ee8136a26c1f3f3f98c3050f8d171
iSamsungS <32791265+iSamsungS@users.noreply.github.com>
2022-03-08 13:46:24 +0200

    eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": ["airbnb-base"],
+    "env": {
+      "node": true,
+      "es6": true,
+      "browser": true
+    },
+    "rules": {
+      "no-console": "on",
+      "no-tabs": "off"
+    }
+  }
