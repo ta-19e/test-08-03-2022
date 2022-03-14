d0b5c8b7ef9a00cdcd911c94955c942991942de7
Siim Tiilen <eritikass@gmail.com>
2022-03-14 08:36:36 +0200

    run eslint and fix errors

--- sum.js
+++ sum.js
@@ -1,11 +1,13 @@
+/* eslint no-restricted-globals: "off" */
+
 function sum(a, b) {
-    if (isNaN(a)) {
-        throw new Error("first parameeter is not number")
-    }
-    if (isNaN(b)) {
-        throw new Error("2nd parameeter is not number")
-    }
-    return a + b;
+  if (isNaN(a)) {
+    throw new Error('first parameeter is not number');
+  }
+  if (isNaN(b)) {
+    throw new Error('2nd parameeter is not number');
+  }
+  return a + b;
 }
 
-module.exports = sum
+module.exports = sum;
