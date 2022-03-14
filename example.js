d9e02303eee8ed3f215ae7cebc9aa5c3b3196a97
Siim Tiilen <eritikass@gmail.com>
2022-03-14 08:39:25 +0200

    set eslint rules for 10

--- example.js
+++ example.js
@@ -1,15 +1,15 @@
 /* eslint no-console: "off" */
 
-const chalk = require('chalk');
-const assert = require('assert');
+const chalk = require("chalk");
+const assert = require("assert");
 
-const sum = require('./sum');
+const sum = require("./sum");
 
 // test how function works
 console.log(sum(1, 3));
 // use also colors to be more "fancy"
-console.log(chalk.yellow('2 + 3 ='), chalk.bold.green(sum(2, 3)));
-console.log(chalk.yellow('9 + -5 ='), chalk.bold.green(sum(9, -5)));
+console.log(chalk.yellow("2 + 3 ="), chalk.bold.green(sum(2, 3)));
+console.log(chalk.yellow("9 + -5 ="), chalk.bold.green(sum(9, -5)));
 
 // examples of testing using assert
 assert.equal(sum(2, 3), 5);
--- sum.js
+++ sum.js
@@ -2,10 +2,10 @@
 
 function sum(a, b) {
   if (isNaN(a)) {
-    throw new Error('first parameeter is not number');
+    throw new Error("first parameeter is not number");
   }
   if (isNaN(b)) {
-    throw new Error('2nd parameeter is not number');
+    throw new Error("2nd parameeter is not number");
   }
   return a + b;
 }
